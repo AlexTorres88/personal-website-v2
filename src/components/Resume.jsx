1b6adf87342b99fe565636c430c82424ace2f8b3
Alex Torres <dalextor@gmail.com>
2022-06-20 20:37:46 -0700

    Feature: add microsoft experience

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import '../assets/modules/home/Home.css'
 const Resume = () => {  
     return (
         <div className="resume-wrapper">
-            <iframe title="resume" src="https://onedrive.live.com/embed?cid=8F3E36A94BC91E9A&resid=8F3E36A94BC91E9A%211317&authkey=ALaOFKebX0T3qr8&em=2" width="100%" height="85%" frameborder="0" scrolling="yes"></iframe>
+            <iframe title="resume" src="https://1drv.ms/b/s!ApoeyUupNj6PiW78QNKCbIBbtfwq" width="100%" height="85%" frameborder="0" scrolling="yes"></iframe>
         </div>
     );
 }
--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -5,11 +5,14 @@ import Card from './WorkCard';
 import { Link } from 'react-router-dom';
 import Konfio from '../assets/WorkExperience/konfio-logo.png'
 import Kinedu from '../assets/WorkExperience/kinedu-logo.png'
+import Microsoft from '../assets/WorkExperience/microsoft-logo.png'
 
 const WorkExperience = () => {
     return (
         <div className="work-exp-container">
-            <Card link="https://konfio.mx" company="Konfío" title=" - Software Engineer Intern" date="August 2021 - Current" desc="Currently working with React, Typescript and Flask. I create new features, refactor code and fix bugs on the account creation team." logo={Konfio} class="work-exp-item"/>
+            <Card link="https://microsoft.com" company="Microsoft" title=" - Software Engineer Intern" date="June 2022 - Current" desc="Currently working with React and Typescript creating new features for the Azure Stack Portal Team" logo={Microsoft} class="work-exp-item"/>
+            <CircleOutlinedIcon className="circle"/>
+            <Card link="https://konfio.mx" company="Konfío" title=" - Software Engineer Intern" date="August 2021 - April 2022" desc="Worked with React and Typescript. I created new features, refactored code and fixed bugs on the account creation team." logo={Konfio} class="work-exp-item"/>
             <CircleOutlinedIcon className="circle"/>
             <Card link="https://app.kinedu.com/" company="Kinedu" title=" - Software Engineer Intern" date="February - August 2021" desc="Worked with Ruby on Rails on the backend team developing new features for the main REST API which served a web and mobile application." logo={Kinedu} class="work-exp-item"/>
             <CircleOutlinedIcon className="circle"/>
